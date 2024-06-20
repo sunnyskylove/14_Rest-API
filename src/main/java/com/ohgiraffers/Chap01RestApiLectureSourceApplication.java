b5ec88f674ab4ac99a12ae095a882f334725e2af
YS <sunnyskylove22@gmail.com>
2024-06-20 12:03:30 +0900

    24.06.20 RestAPI

---
+++ src/main/java/com/ohgiraffers/Chap01RestApiLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
+
+@SpringBootApplication
+public class Chap01RestApiLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap01RestApiLectureSourceApplication.class, args);
+    }
+}
